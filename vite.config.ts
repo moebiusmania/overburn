89c30800546cb289624dff2c912f02b88c379fe5
moebiusmania <moebiusmania@gmail.com>
2024-01-21 14:17:12 +0100

    fix: typos in text, links in 2004 website

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,16 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "2004/index.html"),
+        og: resolve(__dirname, "2004/index.html"),
+        ogBio: resolve(__dirname, "2004/bio.html"),
+        ogDisco: resolve(__dirname, "2004/disco.html"),
+        ogFoto: resolve(__dirname, "2004/foto.html"),
+        ogGig: resolve(__dirname, "2004/gig.html"),
+        ogLink: resolve(__dirname, "2004/link.html"),
+        ogMedia: resolve(__dirname, "2004/media.html"),
+        ogNews: resolve(__dirname, "2004/news.html"),
+        ogStandard: resolve(__dirname, "2004/standard.html"),
+        ogZebo: resolve(__dirname, "2004/zebo.html"),
       },
     },
   },
