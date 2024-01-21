be3dc483c39fe62c80c82aa8387d01ee9da9e494
moebiusmania <moebiusmania@gmail.com>
2024-01-21 13:51:11 +0100

    feat: most of the stuff done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # overburn
+
+> Static website for an ML generated alt-history of one of my previous rock band.
+
+## Built with
+
+- [Vite](https://vitejs.dev/) - scaffolding and basic operations
+- [modern-normalize](https://github.com/sindresorhus/modern-normalize) - web page style reset
+- [ChatGPT](https://chat.openai.com/) v3.5 - generating the made-up story
+- [Midjourney](https://www.midjourney.com/) v6.0 - generating the made-up album covers and website favicon
+- [Github Copilot](https://github.com/features/copilot) - generated most of the webpage CSS styles
+
+## License
+
+Code released under the [MIT license](LICENSE).
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const isProd = process.env.NODE_ENV === "production";
+const base: string | undefined = isProd ? "/overburn/" : "";
 
 export default defineConfig({
+  base,
+  server: {
+    host: true,
+    port: 3000,
+  },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html'),
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "2004/index.html"),
       },
     },
   },
-})
+});
